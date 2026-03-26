6884600a1639ae65bea503c5888883fb25469a2e
Gerli Rand <gerli@hotmail.fi>
2026-03-26 20:20:28 +0200

    Section: Add a new page

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,8 @@ class _MyHomePageState extends State<MyHomePage> {
         break;
       case 1:
         // if you dont use page yet, this handy widget draws a crossed rectangle
-        page = Placeholder();
+        //page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -178,6 +179,35 @@ class BigCard extends StatelessWidget {
           semanticsLabel: "${pair.first} ${pair.second}", // for screen readers
         ),
       ),
+    );
+  }
+}
+
+// Favourites Page
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(child: Text('No favorites yet.'));
+    }
+    // ListView - list word and is scrollable
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            'You have '
+            '${appState.favorites.length} favorites:',
+          ),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
